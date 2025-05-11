5266778ffa0d93926556dde5862c1ffa41439b82
Jose David Narvaez <josedavidnarvaezsoto@gmail.com>
2025-05-10 20:11:54 -0600

    refactor structure

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -1,3 +1,19 @@
+
+type ItemName = 'HealthPotion' | 'Shield' | 'AttackBoost' | 'EnergyDrink' | 'MagicScroll';
 export class Item {
-    
+    name: ItemName;
+    duration: number;
+    effect: string;
+
+    constructor(name: ItemName, effect: string, duration: number = 0) {
+        this.name = name;
+        this.duration = duration
+        this.effect = effect;
+    }
+    useItem() {
+        console.log(`${this.name} used`)
+    }
+    getDescription() {
+        console.log(`Item: ${this.name}, Effect: ${this.effect}, Duration: ${this.duration}`)
+    }
 }
